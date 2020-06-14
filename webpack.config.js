e640cd584599555746ff68c0d63ce13d1f2641f3
Bobby Wang <bobbyquennell@gmail.com>
2020-06-14 20:02:49 +1000

    Add ability to generate index.html automatically
    - manually managing index.html is cumbersome.
    automatically create index.html by using html-webpack-plugin.
    
    follow the guideline : https://webpack.js.org/guides/output-management/
    https://github.com/jantimon/html-webpack-plugin
    
    TODO: inject our own html template in the future

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
@@ -18,6 +18,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Peggy - bootstrap your frontend project', // TODO: create own template later
+      hash: true,
+    }),
+  ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js', 'jsx'],
     modules: [path.resolve(__dirname, 'src'), 'node_modules'], // this is similar to tsconfig: "baseUrl": "src"(https://www.typescriptlang.org/v2/en/tsconfig#baseUrl)
