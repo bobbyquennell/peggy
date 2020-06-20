44e4b868c051e61ce473374faffad2ebbc1b6463
Bobby Wang <bobbyquennell@gmail.com>
2020-06-21 00:13:03 +1000

    Add Style-Loader and CSS loader to apply styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from 'App';
+import './app.css';
 
 ReactDOM.render(<App userName={'Peggy'} />, document.querySelector('body'));
