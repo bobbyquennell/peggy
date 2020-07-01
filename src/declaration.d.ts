388e7885371f4d147c27a1047553da5aa9dea3cd
Bobby Wang <bobbyquennell@gmail.com>
2020-07-02 00:01:38 +1000

    enable ESModule syntax for css module files

---
+++ src/declaration.d.ts
@@ -0,0 +1,24 @@
+// this declaration below is to enable ESModule syntax for css module files:
+// example: import styles from './index.module.css'
+
+// Easy fix is to create global definition (eg. file called typings.d.ts in your source root) for importing CSS Modules:
+// inspired by: https://stackoverflow.com/a/44228423/4390115
+// copied from the file: react-app-env.d.ts from CRA after eject:
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+// for more about using CSS modules with typescript:
+// https://fettblog.eu/typescript-react/styles/
+// https://stackoverflow.com/questions/41336858/how-to-import-css-modules-with-typescript-react-and-webpack
+// https://medium.com/@sapegin/css-modules-with-typescript-and-webpack-6b221ebe5f10
+// https://medium.com/@tommybernaciak/css-modules-for-react-and-typescript-2dd8f0fd7cdd
+// https://ringcentral.github.io/integration-blog/blog/2019/04/03/blog-import-the-style-to-the-typescript-file.html
+// https://stackoverflow.com/a/41946697/4390115
+
+// TODO: in order to support intellisense for class names, consider to use below css module loaders:
+// webpack loaders :
+// https://github.com/TeamSupercell/typings-for-css-modules-loader#readme
+// https://github.com/Jimdo/typings-for-css-modules-loader
+// https://github.com/seek-oss/css-modules-typescript-loader
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,16 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from 'App';
 import './app.css';
-// TODO: enable esmodule syntax for css files: import { redTitle } from './index.module.css'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { redTitle } = require('./index.module.css');
+import styles from './index.module.css';
 
 ReactDOM.render(
   <App
-    className={redTitle}
+    className={styles.redTitle}
     userName={'Peggy'}
     style={{ margin: 20, border: '1px solid black', display: 'flex' }}
   >
     <div className="green-title">Green</div>
-    <div className={redTitle}>Red</div>
+    <div className={styles.redTitle}>Red</div>
   </App>,
   document.querySelector('body'),
 );
