a62ea2ea4143fc91cb2b704d354bd89df7d7a6b2
Bobby Wang <bobbyquennell@gmail.com>
2021-08-08 23:28:43 +1000

    React.FunctionComponent/React.FC is discouraged.
    - check below links for details
    https://github.com/typescript-cheatsheets/react#function-components
    https://github.com/facebook/create-react-app/pull/8177
    https://kentcdodds.com/blog/how-to-write-a-react-component-in-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import greeter from 'greeter';
 import styled from '@emotion/styled';
 interface AppProps extends React.ComponentProps<'div'> {
   userName: string;
-  // children?: React.ReactNode;
+  children: React.ReactNode;
 }
-// type ComponentProps = {
-//   userName: string;
-//   children?: React.ReactNode;
-// } & React.ComponentPropsWithoutRef<'div'>;
+
 const StyledButton = styled.button`
   color: blue;
   background-color: lightcoral;
   height: 5rem;
   width: 10rem;
 `;
-const App: React.FC<AppProps> = ({ userName, children, ...rest }: AppProps) => {
+function App({ userName, children, ...rest }: AppProps) {
   return (
     <div {...rest}>
       <StyledButton>hello, I am from emotion</StyledButton>
@@ -23,6 +20,6 @@ const App: React.FC<AppProps> = ({ userName, children, ...rest }: AppProps) => {
       {children}
     </div>
   );
-};
+}
 
 export default App;
